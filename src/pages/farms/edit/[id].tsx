1c5cabed3958567f81b76e2d3eca3a958bc37719
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-14 14:24:18 -0300

    feat: farm edit page styles

--- src/pages/farms/edit/[id].tsx
+++ src/pages/farms/edit/[id].tsx
@@ -1,11 +1,21 @@
-import { zodResolver } from '@hookform/resolvers/zod'
+import React from 'react'
 import { useRouter } from 'next/router'
-import { useForm, SubmitHandler } from 'react-hook-form'
-import { z } from 'zod'
+import { GetServerSideProps } from 'next'
+
 import { trpc } from '@/utils/trpc'
+import { prisma } from '@/server/db/prisma'
+import { AsyncReturnType } from '@/utils/ts-bs'
+
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import { Container } from '@/components/Container'
+import { Input } from '@/components/Form/Input'
+import { Button } from '@/components/Button'
 
 const farmSchema = z.object({
-  name: z.string(),
+  name: z.string().min(1, { message: 'É preciso de um nome' }),
   payerName: z.string().nullable(),
   payerAddress: z.string().nullable(),
   payerDocument: z.string().nullable(),
@@ -13,16 +23,35 @@ const farmSchema = z.object({
 
 type FarmSchemaType = z.infer<typeof farmSchema>
 
-export default function EditFarm() {
+async function getFarm(id: string) {
+  const farmToEdit = await prisma.farm.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  if (!farmToEdit) {
+    throw new Error('farm does not exists')
+  }
+
+  const farm = farmSchema.parse(farmToEdit)
+
+  return farm
+}
+
+type FarmAsyncResult = AsyncReturnType<typeof getFarm>
+
+const EditFarm: React.FC<{ farm: FarmAsyncResult }> = ({ farm }) => {
   const router = useRouter()
   const { id } = router.query
 
-  const { register, handleSubmit } = useForm<FarmSchemaType>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FarmSchemaType>({
     resolver: zodResolver(farmSchema),
-  })
-
-  const farmToEdit = trpc.getFarmById.useQuery({
-    id: String(id),
+    defaultValues: farm,
   })
 
   const mutation = trpc.editFarm.useMutation()
@@ -37,38 +66,72 @@ export default function EditFarm() {
     router.push('/farms')
   }
 
-  if (farmToEdit.isLoading || !farmToEdit.data) return <h1>Loading...</h1>
-
   return (
-    <form
-      onSubmit={handleSubmit(handleEditFarm)}
-      className="flex flex-col gap-4 [&_input]:bg-slate-200 max-w-xl"
-    >
-      <input
-        type="text"
-        placeholder="Nome Fazenda"
-        defaultValue={farmToEdit.data.name}
-        {...register('name')}
-      />
-      <input
-        type="text"
-        defaultValue={String(farmToEdit.data.payerName)}
-        placeholder="Nome pagador"
-        {...register('payerName')}
-      />
-      <input
-        type="text"
-        placeholder="Endereço"
-        defaultValue={String(farmToEdit.data.payerAddress)}
-        {...register('payerAddress')}
-      />
-      <input
-        type="text"
-        defaultValue={String(farmToEdit.data.payerDocument)}
-        placeholder="Documento"
-        {...register('payerDocument')}
-      />
-      <button type="submit">Edit</button>
-    </form>
+    <div>
+      <Container classNames="mt-[10rem] text-sm">
+        <div className="flex flex-col w-full bg-white rounded-md shadow-header">
+          <h1 className="text-md p-5 font-semibold">Nova fazenda</h1>
+          <form
+            onSubmit={handleSubmit(handleEditFarm)}
+            className="flex w-full flex-col gap-4 px-5 pb-5"
+          >
+            <div className="px-3">
+              <Input
+                label="Fazenda"
+                error={errors.name}
+                type="text"
+                placeholder="Nome Fazenda"
+                {...register('name')}
+              />
+            </div>
+            <div>
+              <h2 className="text-md">Pagador</h2>
+              <div className="px-3 mt-5">
+                <Input
+                  label="Nome"
+                  error={errors.payerName}
+                  type="text"
+                  placeholder="Nome pagador"
+                  {...register('payerName')}
+                />
+                <div className="grid grid-cols-2 gap-5 mt-3">
+                  <Input
+                    label="Endereço"
+                    error={errors.payerAddress}
+                    type="text"
+                    placeholder="Endereço"
+                    {...register('payerAddress')}
+                  />
+                  <Input
+                    label="Documento"
+                    error={errors.payerDocument}
+                    type="text"
+                    placeholder="Documento"
+                    {...register('payerDocument')}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="w-full px-3">
+              <Button type="submit">Editar</Button>
+            </div>
+          </form>
+        </div>
+      </Container>
+    </div>
   )
+}
+
+export default EditFarm
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query
+
+  const farm = await getFarm(id as string)
+
+  return {
+    props: {
+      farm,
+    },
+  }
 }
