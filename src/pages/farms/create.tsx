77340e7f743fd70b56094ad024c35f9b6b819085
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-12-14 14:12:09 -0300

    feat: create farm styles

--- src/pages/farms/create.tsx
+++ src/pages/farms/create.tsx
@@ -3,9 +3,12 @@ import { useRouter } from 'next/router'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { z } from 'zod'
 import { trpc } from '@/utils/trpc'
+import { Container } from '@/components/Container'
+import { Input } from '@/components/Form/Input'
+import { Button } from '@/components/Button'
 
 const farmSchema = z.object({
-  name: z.string(),
+  name: z.string().min(1, { message: 'É preciso de um nome' }),
   payerName: z.string().nullable(),
   payerAddress: z.string().nullable(),
   payerDocument: z.string().nullable(),
@@ -15,7 +18,11 @@ type FarmSchemaType = z.infer<typeof farmSchema>
 
 export default function NewFarm() {
   const router = useRouter()
-  const { register, handleSubmit } = useForm<FarmSchemaType>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FarmSchemaType>({
     resolver: zodResolver(farmSchema),
   })
 
@@ -30,23 +37,57 @@ export default function NewFarm() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(handleCreateFarm)}
-      className="flex flex-col gap-4 [&_input]:bg-slate-200 max-w-xl"
-    >
-      <input type="text" placeholder="Nome Fazenda" {...register('name')} />
-      <input
-        type="text"
-        placeholder="Nome pagador"
-        {...register('payerName')}
-      />
-      <input type="text" placeholder="Endereço" {...register('payerAddress')} />
-      <input
-        type="text"
-        placeholder="Documento"
-        {...register('payerDocument')}
-      />
-      <button type="submit">Criar</button>
-    </form>
+    <div>
+      <Container classNames="mt-[10rem] text-sm">
+        <div className="flex flex-col w-full bg-white rounded-md shadow-header">
+          <h1 className="text-md p-5 font-semibold">Nova fazenda</h1>
+          <form
+            onSubmit={handleSubmit(handleCreateFarm)}
+            className="flex w-full flex-col gap-4 px-5 pb-5"
+          >
+            <div className="px-3">
+              <Input
+                error={errors.name}
+                label="Fazenda"
+                placeholder="Nome Fazenda"
+                {...register('name')}
+                type="text"
+              />
+            </div>
+            <div>
+              <h2 className="text-md">Pagador</h2>
+              <div className="px-3 mt-5">
+                <Input
+                  label="Nome"
+                  error={errors.payerName}
+                  type="text"
+                  placeholder="Nome pagador"
+                  {...register('payerName')}
+                />
+                <div className="grid grid-cols-2 gap-5 mt-3">
+                  <Input
+                    label="Endereço"
+                    error={errors.payerAddress}
+                    type="text"
+                    placeholder="Endereço"
+                    {...register('payerAddress')}
+                  />
+                  <Input
+                    label="Documento"
+                    error={errors.payerDocument}
+                    type="text"
+                    placeholder="Documento"
+                    {...register('payerDocument')}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="w-full px-3">
+              <Button type="submit">Criar</Button>
+            </div>
+          </form>
+        </div>
+      </Container>
+    </div>
   )
 }
