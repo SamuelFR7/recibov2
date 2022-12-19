45313c3c42e92aa0eb1fa5c87571db952594ab5c
Samuel Rezende <samuelferreirarezende@gmail.com>
2022-12-18 23:49:52 -0300

    fix: remove chrome aws lambda

--- src/pages/api/receipts/print/by-farm.ts
+++ src/pages/api/receipts/print/by-farm.ts
@@ -1,14 +1,40 @@
-import { saveAsPdf } from '@/utils/saveAsPdf'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { unstable_getServerSession } from 'next-auth'
+import puppeteer from 'puppeteer'
+import { authOptions } from '../../auth/[...nextauth]'
+
+const saveAsPdf = async (id: string) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.goto(`http://localhost:3000/receipts/print/by-farm/${id}`, {
+    waitUntil: 'networkidle0',
+  })
+
+  const result = await page.pdf({
+    format: 'a4',
+    printBackground: true,
+  })
+
+  await browser.close()
+
+  return result
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await unstable_getServerSession(req, res, authOptions)
+
+  if (!session) {
+    return res.send({
+      message: 'You are not authorized to access this content',
+    })
+  }
+
   const { id } = req.query
 
   res.setHeader('Content-Type', 'application/pdf')
 
-  const pdf = await saveAsPdf(
-    `http://localhost:3000/receipts/print/by-farm/${id}`,
-  )
+  const pdf = await saveAsPdf(id as string)
 
   return res.send(pdf)
 }
