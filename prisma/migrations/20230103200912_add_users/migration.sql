72e878220d6100a8bf0c70c69913f33c0c3540b9
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-01-03 17:09:23 -0300

    feat: update users migration

--- prisma/migrations/20230103200912_add_users/migration.sql
+++ prisma/migrations/20230103200912_add_users/migration.sql
@@ -2,10 +2,15 @@
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
+    "email" TEXT,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
