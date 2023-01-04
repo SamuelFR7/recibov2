52a3319f5d7ada06af2ac08ccea99464526a119e
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-01-04 09:35:50 -0300

    fix: update env checker

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -4,6 +4,14 @@ import { z } from 'zod'
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   JWT_SECRET: z.string(),
+  NEXTAUTH_SECRET:
+    process.env.NODE_ENV === "production"
+      ? z.string().min(1)
+      : z.string().min(1).optional(),
+  NEXTAUTH_URL: z.preprocess(
+    (str) => process.env.VERCEL_URL ?? str,
+    process.env.VERCEL ? z.string() : z.string().url(),
+  ),
   NODE_ENV: z.enum(['development', 'test', 'production']),
 })
 
